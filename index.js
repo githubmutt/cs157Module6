4e045244a771c5a9a4692b565e47546c55a660bc
githubmutt <97822530+githubmutt@users.noreply.github.com>
2022-10-16 03:50:22 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const employeeDatabase = require("./employeeDatabase.js");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.post("/api/employees/login", (req, res) => {
+  let emp = employeeDatabase.authenticateEmployee(req.body.email, req.body.password);
+
+  if(emp) {
+    return res.status(200).send(emp);
+  } else {
+    return res.status(400).send("Invalid username/password.");
+  }
+});
+
+
+app.patch("/api/employees/:id", (req, res) => {
+  let id = parseInt(req.params.id, 10);
+
+  if (!isNaN(id)) {
+    let emp = employeeDatabase.updateEmployee(id, req.body);
+    if (emp) {
+      res.status(200).send(emp);
+    } else {
+      res.status(404).send("Employee Not Found.");
+    }
+  } else {
+    res.status(400).send("Invalid Employee ID.")
+  }
+
+});
+
+
+/*
+  PART 4 EXTRA CREDIT 
+  DELETE /api/employees/:id handler here use the employeeDatabase.deleteEmployee(id)
+  function to delete the employee from the database.
+*/
+
+
+app.listen(3000, () => console.log('server started'));
